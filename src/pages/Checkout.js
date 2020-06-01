f1cf7dbc5a81ff3a013828bb8f32a2336c0270f2
Nico <nicosiput@gmail.com>
2020-06-01 16:38:22 +0700

    Integration API Detail Page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -46,7 +46,7 @@ class Checkout extends Component {
 
   render() {
     const { data } = this.state;
-    const { checkout } = this.props;
+    const { checkout, page } = this.props;
 
     if (!checkout)
       return (
@@ -58,7 +58,12 @@ class Checkout extends Component {
             <div className="col-3">
               Pilih kamar dulu
               <div>
-                <Button className="btn mt-5" type="link" href="/" isLight>
+                <Button
+                  className="btn mt-5"
+                  type="button"
+                  onClick={() => this.props.history.goBack()}
+                  isLight
+                >
                   Back
                 </Button>
               </div>
@@ -66,6 +71,7 @@ class Checkout extends Component {
           </div>
         </div>
       );
+
     const steps = {
       bookingInformation: {
         title: "Booking Information",
@@ -74,7 +80,7 @@ class Checkout extends Component {
           <BookingInformation
             data={data}
             checkout={checkout}
-            ItemDetails={ItemDetails}
+            ItemDetails={page[checkout._id]}
             onChange={this.onChange}
           />
         ),
@@ -86,7 +92,7 @@ class Checkout extends Component {
           <Payment
             data={data}
             checkout={checkout}
-            ItemDetails={ItemDetails}
+            ItemDetails={page[checkout._id]}
             onChange={this.onChange}
           />
         ),
@@ -201,5 +207,6 @@ class Checkout extends Component {
 
 const mapStateToProps = (state) => ({
   checkout: state.checkout,
+  page: state.page,
 });
 export default connect(mapStateToProps)(Checkout);
